68402e16512e838184f61258be75c03fac14395f
NaotakaSaito <saitou088@dsn.lapis-semi.com>
2017-11-22 18:19:34 +0900

    update node-factory

--- nodes/node-factory/node-factory.js
+++ nodes/node-factory/node-factory.js
@@ -15,17 +15,29 @@
  **/
 
 module.exports = function(RED) {
+	var param = require('../../param');
+	var os = require('os');
+	var ready = false;
+	//var qs = require('querystring');
+	function isConnect(awsiot){
+		var result = true;
+		for(key in awsiot){
+			if(!RED.settings.get('aws-iot'+awsiot[key])) {result = false;};
+		}
+		return result;
+	}
 	function NodeFactory(config) {
 		WARN = require('./warning_code.js')
 		RED.nodes.createNode(this,config);
 		var node = this;
 		var outMsg = [];
 		node.config = RED.nodes.getNode(config.config).config;
-
 		node.capacity = [];
 		node.worklog = [];
+		node.initialized = false;
+		node.awsiot = [];
 
-		// convert address 
+		// convert address
 		node.config.rules.forEach(function(data){
 			var src_ct,src_hall;
 			if(data.ct === null || data.ct === undefined || data.ct === ""){
@@ -49,20 +61,24 @@ module.exports = function(RED) {
 					[parseInt(data.hall),0,0,0];
 			}
 			node.capacity.push({
+				id: parseInt(data.id),					// source address of CT
 				ct_addr: src_ct,						// source address of CT
-				ct_total: 0,							// total meas count of CT
-				ct_active: 0,							// active count of CT
-				//ct_stop: 0,								// stop count of CT
-				ct_wait: 0,								// wait count of CT
-				ct_wait_time: 0,						// wait count of CT
-				ct_thres: parseFloat(data.thres),		// threshold of CT
 				ct_state: undefined,					// status of CT
 				ct_state_time: undefined,				// time of changing state
+				ct_total_time: 0,						// total operating time
+				ct_active_time: 0,						// total active time
+				ct_wait_time: 0,						// time of starting wait
+				ct_last_time: 0,						// time of last tx
+				count: 0,								// count of receiving
+				rssi: 0,								// latest rssi
+				vbat: 0,								// battery voltage
+				ct_thres: parseFloat(data.thres),		// threshold of CT
 				hall_addr: src_hall,					// src addr of hall sensor
 				hall_state: undefined,					// state of CT sensor
 				hall_state_time: undefined,				// time of changing hall state
 				hall_warn: false,						// warning state(true: triggered, false: no)
 				ct_dead_time: parseFloat(data.detect)*1000,	// dead_time(ms) of wait
+				update: false,
 			});
 			node.worklog.push({
 				time: new Array(4) });
@@ -74,17 +90,31 @@ module.exports = function(RED) {
 			reset_time: undefined			// interval of reset capacity
 		};
 		node.on('input', function (msg) {
-			outMsg = Array(4);
-			if(msg.payload instanceof Array) {
-				msg.payload.forEach(function(data) {
-					data.payload = data.payload.split('\n')[0];
-					calCapacity(data);
-					node.send(outMsg);
-				});
+			if(node.initialized == false) {
+				for(i1 in node.wires) {
+					for(i2 in node.wires[i1]){
+						if(typeof RED.settings.get('aws-iot'+node.wires[i1][i2]) !== 'undefined') {
+							node.awsiot.push(node.wires[i1][i2]);
+						}
+					}
+				}
+				node.initialized = true;
+			}
+			if(isConnect(node.awsiot)){
+				outMsg = Array(4);
+				if(msg.payload instanceof Array) {
+					msg.payload.forEach(function(data) {
+						data.payload = data.payload.split('\n')[0];
+						calCapacity(data);
+						node.send(outMsg);
+					});
+				} else {
+						msg.payload = msg.payload.split('\n')[0];
+						calCapacity(msg);
+						node.send(outMsg);
+				}
 			} else {
-					msg.payload = msg.payload.split('\n')[0];
-					calCapacity(msg);
-					node.send(outMsg);
+				console.log('aws-iot disconnected');
 			}
 		});
 
@@ -99,6 +129,9 @@ module.exports = function(RED) {
 						// calcurate rx time
 						rxtime = msg.sec*1000 + Math.round(msg.nsec /1000000);
 						var value = parseFloat(msg.payload.split(',')[0]);
+						node.capacity[i].count++;
+						node.capacity[i].rssi = msg.rssi;
+						node.capacity[i].vbat = msg.payload.split(',')[1];
 						ctState(i,value,rxtime);
 						break;
 					}
@@ -135,9 +168,7 @@ module.exports = function(RED) {
 					updateWorkLog(index,'open',rxtime);
 					node.capacity[index].ct_state = 'stop';
 					node.capacity[index].ct_state_time = node.capacity[index].ct_wait_time;
-					node.capacity[index].ct_total += node.capacity[index].ct_wait;
-					//node.capacity[index].ct_stop  += node.capacity[index].ct_wait;
-					node.capacity[index].ct_wait = 0;
+					node.capacity[index].ct_total_time += (rxtime - node.capacity[index].ct_wait_time);
 				} else if(node.capacity[index].ct_state == 'stop') {
 					updateWorkLog(index,'open',rxtime);
 					// move to stop state in force.
@@ -161,20 +192,20 @@ module.exports = function(RED) {
 
 		// function to calcurate capacity
 		function ctState(index,data,rxtime){
-			var current_time = new Date(rxtime);
+			var delta_time = rxtime - node.capacity[index].ct_last_time;
 			updateCapInterval(rxtime);
 			switch(node.capacity[index].ct_state){
 			case undefined:
 			case null:						// first time
-				node.capacity[index].ct_total += 1;
+				node.capacity[index].ct_active_time= 0;
+				node.capacity[index].ct_total_time = 0;
+				node.capacity[index].ct_state_time = rxtime;
 				if(data > node.capacity[index].ct_thres) {
 					node.capacity[index].ct_state = 'act';
-					node.capacity[index].ct_active += 1;
-					node.capacity[index].ct_state_time = rxtime;
+					updateWorkLog(index,'act',rxtime);
 				} else {
-					updateWorkLog(index,'wait',rxtime);
 					node.capacity[index].ct_state = 'stop';
-					node.capacity[index].ct_state_time = rxtime;
+					updateWorkLog(index,'stop',rxtime);
 				}
 				break;
 			case 'act':						// current is active
@@ -194,44 +225,41 @@ module.exports = function(RED) {
 				}
 				// 
 				if(data > node.capacity[index].ct_thres) {
-					node.capacity[index].ct_total += 1;
-					node.capacity[index].ct_active += 1;
+					node.capacity[index].ct_total_time += delta_time;
+					node.capacity[index].ct_active_time += delta_time;
 				} else {
 					updateWorkLog(index,'wait',rxtime);
 					node.capacity[index].ct_state = 'wait';
-					node.capacity[index].ct_wait = 1;
-					node.capacity[index].ct_wait_time = rxtime;
+					node.capacity[index].ct_wait_time = node.capacity[index].ct_last_time;
 				}
+				if(node.capacity[index].update) {updateWorkLog(index,'act',rxtime);}
 				break;
 			case 'stop':					// current is stop
-				node.capacity[index].ct_total += 1;
+				node.capacity[index].ct_total_time += delta_time;
 				if(data > node.capacity[index].ct_thres) {
-					updateWorkLog(index,'act',rxtime);
 					node.capacity[index].ct_state = 'act';
-					node.capacity[index].ct_active += 1;
+					node.capacity[index].ct_active_time += delta_time;
 					node.capacity[index].ct_state_time = rxtime;
-				} else {
-					//node.capacity[index].ct_stop += 1;
-				}
+					updateWorkLog(index,'act',rxtime);
+				} 
+				if(node.capacity[index].update) {updateWorkLog(index,'stop',rxtime);}
 				break;
 			case 'wait':					// dead time from current stop to stop state
 				// restart during dead time
 				if(data > node.capacity[index].ct_thres) {
-					updateWorkLog(index,'reset',rxtime);
 					node.capacity[index].ct_state = 'act';
-					node.capacity[index].ct_total += (node.capacity[index].ct_wait + 1);
-					node.capacity[index].ct_active += (node.capacity[index].ct_wait + 1);
-					node.capacity[index].ct_wait = 0;
+					node.capacity[index].ct_total_time  += (rxtime - node.capacity[index].ct_wait_time);
+					node.capacity[index].ct_active_time += (rxtime - node.capacity[index].ct_wait_time);
+					node.capacity[index].ct_wait_time = 0;
 				} else {
 					// move to stop state
 					if((rxtime - node.capacity[index].ct_wait_time)>node.capacity[index].ct_dead_time){
 						node.capacity[index].ct_state = 'stop';
+						node.capacity[index].ct_total_time  += (rxtime - node.capacity[index].ct_wait_time);
 						node.capacity[index].ct_state_time = node.capacity[index].ct_wait_time;
-						node.capacity[index].ct_total += (node.capacity[index].ct_wait + 1);
-						//node.capacity[index].ct_stop  += (node.capacity[index].ct_wait + 1);
-						node.capacity[index].ct_wait = 0;
+						node.capacity[index].ct_wait_time = 0;
+						updateWorkLog(index,'stop',rxtime);
 					} else {
-						node.capacity[index].ct_wait += 1;
 					}
 				}
 				break;
@@ -241,9 +269,10 @@ module.exports = function(RED) {
 				topic: node.config.rules[index].machine,
 				state: node.capacity[index].ct_state,
 				interval: intervalFormatter(rxtime - node.capacity[index].ct_state_time),
-				payload: ((node.capacity[index].ct_active / node.capacity[index].ct_total)*100).toFixed(2),
+				payload: ((node.capacity[index].ct_active_time / node.capacity[index].ct_total_time)*100).toFixed(2),
 			};
 			outMsg[0] = msg;
+			node.capacity[index].ct_last_time = rxtime;
 			return;
 		}
 		// generate worklog
@@ -270,27 +299,21 @@ module.exports = function(RED) {
 					node.worklog[index].time[1] = node.worklog[index].time[0];
 				}
 				break;
+			case 'stop':
 			case 'act':
 				// restarting
 				node.worklog[index].time[3] = rxtime;
 				// message of worklog
-				msg.payload = [
-					Math.round(node.worklog[index].time[0]/1000),
-					dateFormatter(node.worklog[index].time[0]),
-					isNaN(node.worklog[index].time[1]) ? "" : Math.round(node.worklog[index].time[1]/1000),
-					dateFormatter(node.worklog[index].time[1]),
-					isNaN(node.worklog[index].time[2]) ? "" : Math.round(node.worklog[index].time[2]/1000),
-					dateFormatter(node.worklog[index].time[2]),
-					Math.round(node.worklog[index].time[3]/1000),
-					dateFormatter(node.worklog[index].time[3]),
-					intervalFormatter(node.worklog[index].time[3]-node.worklog[index].time[0]),
-					intervalFormatter(node.worklog[index].time[1]-node.worklog[index].time[0]),
-					intervalFormatter(node.worklog[index].time[2]-node.worklog[index].time[1]),
-					"",
-					node.config.rules[index].machine
-				];
+				msg.payload = {
+					timestamp: rxtime,			// id of database
+					state:     state,
+					from:      node.capacity[index].ct_state_time,
+					machine:   node.capacity[index].id,
+					type:      'log'
+				};
 				outMsg[2] = msg;
 				node.worklog[index].time = new Array(4);
+				node.capacity[index].update = false;
 				break;
 			}
 		}
@@ -298,6 +321,8 @@ module.exports = function(RED) {
 		// function for interval process
 		function updateCapInterval(rxtime){
 			var st_meas_time,st_reset_time,now_meas_time, now_reset_time;
+			var type;
+			var flag = false;
 			if(node.capInterval.meas_time === undefined) {
 				node.capInterval.meas_time = rxtime;
 			}
@@ -307,18 +332,21 @@ module.exports = function(RED) {
 			// generate comparing time according to interval
 			switch(node.capInterval.interval) {
 			case 0:			//minuites
+				type = 'min';
 				st_meas_time = new Date(node.capInterval.meas_time).getMinutes();
 				st_reset_time = st_meas_time;
 				now_meas_time = new Date(rxtime).getMinutes();
 				now_reset_time = now_meas_time;
 				break;
 			case 1:			// hour
+				type = 'hour';
 				st_meas_time = new Date(node.capInterval.meas_time).getHours();
 				st_reset_time = st_meas_time;
 				now_meas_time = new Date(rxtime).getHours();
 				now_reset_time = now_meas_time;
 				break;
 			case 2:			// day
+				type = 'day';
 				st_meas_time = new Date(node.capInterval.meas_time).getHours();
 				st_reset_time = new Date(node.capInterval.meas_time).getDate();
 				now_meas_time = new Date(rxtime).getHours();
@@ -329,34 +357,59 @@ module.exports = function(RED) {
 				now_meas_time = new Date(rxtime).getHours();
 				break;
 			}
-
 			// function to report per interval
+			var now = new Date();
 			if(st_meas_time != now_meas_time) {
-				var msg = new Array(node.capacity.length + 1);
-				if(node.capInterval.interval == 0) {
-					msg[0] = dateFormatterMinute(node.capInterval.meas_time);
-				} else {
-					msg[0] = dateFormatterHour(node.capInterval.meas_time);
-				}
+				var payload = {
+					timestamp: now.getTime(),
+					type: type,
+					id: param.lot,
+				};
+				payload.network = getLocalAddress("ppp0")
+				payload.capacity = {};
+				payload.count = {};
+				payload.rssi = {};
+				payload.vbat = {};
+				//console.log({capacity:node.capacity,config:node.config.rules,param:param});
 				for(var i = 0; i < node.capacity.length ; i++) {
-					msg[i+1] = ((node.capacity[i].ct_active / node.capacity[i].ct_total)*100).toFixed(2);
+					if(node.capacity[i].count > 0) {
+						payload.count[node.capacity[i].id] = node.capacity[i].count;
+						payload.rssi[node.capacity[i].id] = node.capacity[i].rssi;
+						payload.vbat[node.capacity[i].id] = node.capacity[i].vbat;
+						if(node.capacity[i].ct_total_time != 0){
+							payload.capacity[node.capacity[i].id] = parseInt((node.capacity[i].ct_active_time/node.capacity[i].ct_total_time*10000))/100;
+						} else {
+							payload.capacity[node.capacity[i].id] = 0;
+						}
+					}
+					if(now_meas_time == 0){
+						node.capacity[i].update = true;
+						console.log('reset ct_state',i);
+					}
 				}
-				outMsg[1] = { payload: msg };
+				outMsg[1] = {
+					payload: payload,
+				};
+				console.log({outMsg1: outMsg[1]});
 				node.capInterval.meas_time = rxtime;
+				flag = true;
 			}
 			
 			// function to reset per interval
 			if(node.capInterval.interval < 3) {
 				if(st_reset_time !== now_reset_time){
-					for(var i = 0; i < node.capacity.length; i++) {
+					for(i in node.capacity) {
 						// reset data
-						node.capacity[i].ct_total = 0;
-						node.capacity[i].ct_active = 0;
+						node.capacity[i].ct_total_time = 0;
+						node.capacity[i].ct_active_time = 0;
+						node.capacity[i].count = 0;
+						node.capacity[i].rssi = 0;
+						node.capacity[i].vbat = 0;
 					}
 					node.capInterval.reset_time = rxtime;
 				}
 			}
-
+			return flag;
 		}
 		function dateFormatter(time) {
 			if(isNaN(time)) return "";
@@ -391,6 +444,21 @@ module.exports = function(RED) {
 			var min = Math.floor(sec/60);
 			sec = sec % 60; 
 			return hour+"時間"+min+"分"+sec+"秒";
+		}
+		function addr64(_data){
+			var _tmp =0;
+			for(var i=3;i>=0;i--){
+				_tmp = _tmp * 65536 + _data[i];
+			}
+			return _tmp;
+		}
+		function getLocalAddress(adaptor){
+			var address = os.networkInterfaces();
+			if(address[adaptor].length == 1){
+				return address[adaptor][0].address;
+			} else {
+				return "unknown";
+			}
 		}
 	}
 	
